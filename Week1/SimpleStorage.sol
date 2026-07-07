50463fe5b044d24506c5644e33167d188edd033d
AnuragReddy <motheanuragreddy23@gmail.com>
2026-07-07 12:22:20 +0530

    Create SimpleStorage.sol

---
+++ Week1/SimpleStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+contract SimpleStorage{
+    uint256 private store;
+    function set(uint256  _num) public {
+        store=_num;
+    }
+    function get() public view returns(uint256){
+        return store;
+    }
+    
+}
