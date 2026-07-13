4f24cb7dc6fb018a9ad77987cd91cf26112c52bf
AnuragReddy <motheanuragreddy23@gmail.com>
2026-07-13 22:58:16 +0530

    Update Task Complition token.sol

--- Week2/Task Complition token.sol
+++ Week2/Task Complition token.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TaskCompletionToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Task Completion Token", "TCT") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply * (10 ** decimals()));
+    }
+    // ...
+
+
+    // Mint new tokens (only owner)
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Burn tokens (only owner)
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}
 
