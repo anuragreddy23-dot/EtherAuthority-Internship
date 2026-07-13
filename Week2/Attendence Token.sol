6db7d272da1d3343db062ca70a3cb103f4b37480
AnuragReddy <motheanuragreddy23@gmail.com>
2026-07-13 23:02:50 +0530

    Update Attendence Token.sol

--- Week2/Attendence Token.sol
+++ Week2/Attendence Token.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract AttendanceToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Attendance Token", "ATN") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply * (10 ** decimals()));
+    }
+    // ... rest of the code
+    // Mint new tokens (only owner)
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Burn tokens (only owner)
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}
 
