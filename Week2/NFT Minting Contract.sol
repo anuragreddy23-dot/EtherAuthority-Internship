a923fc07b0dd352aa6f7ff34041faaabe70232af
AnuragReddy <motheanuragreddy23@gmail.com>
2026-07-18 14:05:05 +0530

    Update NFT Minting Contract.sol

--- Week2/NFT Minting Contract.sol
+++ Week2/NFT Minting Contract.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTMinting is ERC721 {
+    uint256 public nextTokenId;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function mint() public {
+        _safeMint(msg.sender, nextTokenId);
+        nextTokenId++;
+    }
+}
