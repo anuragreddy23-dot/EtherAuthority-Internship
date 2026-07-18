856925a81cb3eb4949b4fafbf749c6aafc6f38e5
AnuragReddy <motheanuragreddy23@gmail.com>
2026-07-18 13:59:46 +0530

    Update Achivement Badge.sol

--- Week2/Achivement Badge.sol
+++ Week2/Achivement Badge.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AchievementBadgeNFT is ERC721, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(address initialOwner)
+        ERC721("Achievement Badge", "ABNFT")
+        Ownable(initialOwner)
+    {}
+
+    function mint(address to) public onlyOwner {
+        _safeMint(to, _nextTokenId);
+        _nextTokenId++;
+    }
+}
